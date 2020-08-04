b370b821a855d830eea0ab28b469afafb59ba4ba
LuizFernando55 <45644952+LuizFernando55@users.noreply.github.com>
2020-08-04 14:05:30 -0300

    init First Test

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest {
+    [TestClass]
+    public class FileProcessTest {
+        private const string BAD_FILE_NAME = @"C:\BadFileName.bat";
+        private string _GoodFileName;
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void FileNameDoesExists() {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            SetGoodFileName();
+            
+            TestContext.WriteLine($"Creating File: {_GoodFileName}");
+            File.AppendAllText(_GoodFileName, "Some Text");
+            
+            TestContext.WriteLine($"Testing File: {_GoodFileName}");
+            fromCall = fp.FileExists(_GoodFileName);
+
+            TestContext.WriteLine($"Deleting File: {_GoodFileName}");
+            File.Delete(_GoodFileName);
+
+            Assert.IsTrue(fromCall);
+        }
+
+        public void SetGoodFileName() {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if(_GoodFileName.Contains("[AppPath]")) {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists() {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(BAD_FILE_NAME);
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException() {
+            // TODO;
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingTryCatch() {
+            // TODO;
+            FileProcess fp = new FileProcess();
+
+            try {
+                fp.FileExists("");
+            }
+            catch (ArgumentException) {
+                // The test was a sucess;
+                return;
+            }
+
+            Assert.Fail("Fail expected");
+        }
+    }
+}
---
+++ MyClassesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyClassesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f857543e-b4cb-4ae4-933e-3049fecffec3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
